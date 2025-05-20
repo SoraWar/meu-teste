3eb5fc4300656ed6237d74f97a2d9f0bcf2f3f52
SoraWar <wesley.fsouza21@gmail.com>
2025-05-20 06:07:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ function enviarDados() {
   const inputs = document.querySelectorAll(".valor");
   const valores = Array.from(inputs).map(input => input.value);
 
-  fetch("https://script.google.com/macros/s/AKfycbwJtVYn8exZHnxOtmom5hH8reh9s0FGDXv4szCctZNfamuZWcm7iLOc3vJMyW-_yjQ5/exec", {
-    method: "POST",
-    body: JSON.stringify({
-      codigo: codigo,
-      valores: valores
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
+  // Monta os parâmetros da URL
+  const params = new URLSearchParams();
+  params.append("codigo", codigo);
+  valores.forEach((v, i) => params.append(`valor${i + 1}`, v));
+
+  // URL do Google Apps Script
+  const url = "https://script.google.com/macros/s/AKfycbwlUrhVVN1xX15lZrUKLFpb22e73oUCiggKdSO6JOyxtgXddPk7FWe-gZy_stESh6sX/exec" + params.toString();
+
+  // Envia os dados
+  fetch(url)
     .then(response => response.text())
     .then(data => {
       alert("Dados enviados com sucesso!");
