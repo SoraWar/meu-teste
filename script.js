77a3afc3037692d0c79d52a7ac89887396fa8d61
SoraWar <wesley.fsouza21@gmail.com>
2025-05-22 14:52:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-async function enviarDados() {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzKvWtmaQGbkIB1G5PfPanV0vwamMeS-c",
+  authDomain: "apptest-1b228.firebaseapp.com",
+  databaseURL: "https://apptest-1b228-default-rtdb.firebaseio.com",
+  projectId: "apptest-1b228",
+  storageBucket: "apptest-1b228.firebasestorage.app",
+  messagingSenderId: "408356425490",
+  appId: "1:408356425490:web:3573237d22e83baf4f2dcb"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+document.getElementById('meuFormulario').addEventListener('submit', function(e) {
+  e.preventDefault();
+
   const codigo = document.getElementById("codigo").value;
-  const valores = Array.from(document.querySelectorAll(".valor")).map(input => input.value);
+  const valores = [
+    document.getElementById("valor1").value,
+    document.getElementById("valor2").value,
+    document.getElementById("valor3").value,
+    document.getElementById("valor4").value,
+    document.getElementById("valor5").value,
+    document.getElementById("valor6").value
+  ];
 
-  const dados = {
-    codigo: codigo,
-    valores: valores
-  };
-
-  try {
-    const resposta = await fetch("https://script.google.com/macros/s/AKfycbwlUrhVVN1xX15lZrUKLFpb22e73oUCiggKdSO6JOyxtgXddPk7FWe-gZy_stESh6sX/exec", {
-      method: "POST",
-      body: JSON.stringify(dados),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
-
-    const texto = await resposta.text();
-    alert(texto);
-  } catch (erro) {
-    alert("Erro ao enviar dados: " + erro.message);
-  }
-}
-
+  push(ref(db, 'dados/'), {
+    codigo,
+    valores
+  })
+  .then(() => alert("Dados enviados com sucesso!"))
+  .catch((err) => alert("Erro: " + err.message));
+});
