0a4833189d12bb4d7c6a4f3924f733473b2b2482
SoraWar <wesley.fsouza21@gmail.com>
2025-06-05 12:21:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ function enviarDados() {
   const valores = Array.from(document.querySelectorAll('.valor')).map(input => input.value);
 
   fetch('https://script.google.com/macros/s/AKfycbwlUrhVVN1xX15lZrUKLFpb22e73oUCiggKdSO6JOyxtgXddPk7FWe-gZy_stESh6sX/exec', {
+    redirect: "follow",
     method: 'POST',
-    mode: 'no-cors', // importante para evitar CORS
-    headers: {
-      'Content-Type': 'application/json'
-    },
     body: JSON.stringify({
       codigo: codigo,
       valores: valores
-    })
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    
   })
   .then(() => {
     alert('Dados enviados com sucesso!');
